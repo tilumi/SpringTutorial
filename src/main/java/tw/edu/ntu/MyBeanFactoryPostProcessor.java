435d487acd9855dde7e272b9e624494abf30c6bf
Lucas Yang <lucasmf@yahoo-inc.com>
2015-02-02 11:01:21 +0800

    init

---
+++ src/main/java/tw/edu/ntu/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package tw.edu.ntu;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * @author migue
+ *
+ */
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor{
+
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+			throws BeansException {
+			System.out.println("My Bean Factory Post Processor called");
+		
+	}
+
+}
